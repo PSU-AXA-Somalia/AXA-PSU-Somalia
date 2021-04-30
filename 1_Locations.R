cd32efeb1f7361e28020837577d2e0b6faba8bf1
hgreatrex <5038576+hgreatrex@users.noreply.github.com>
2021-04-30 00:20:33 -0400

    Add in revamped templates

--- 1_Locations.R
+++ 1_Locations.R
@@ -16,18 +16,21 @@ computer_sep <- "/"
 #--------------------------------------------------------
 
 # Top level
-dir_0main <- "."  
-dir_Code   <- paste(dir_0main,"0_Code",sep=computer_sep)
-dir_Output <- paste(dir_0main,"0_Output",sep=computer_sep)
+dir_0main           <- "."  
+dir_Code            <- paste(dir_0main,"0_Code",sep=computer_sep)
+dir_Output          <- paste(dir_0main,"..","AXA-PSU-Output",sep=computer_sep)
+
 
 # Code folder tree - hardwired
- dir_Code_Download   <- paste(dir_Code, "0_Download",sep=computer_sep) 
- dir_Code_Extract    <- paste(dir_Code, "1_Extract",sep=computer_sep) 
- dir_Code_Analysis   <- paste(dir_Code, "2_Analysis",sep=computer_sep) 
- dir_Code_Dashboard  <- paste(dir_Code, "3_Dashboard",sep=computer_sep) 
+ dir_Output_template <- paste(dir_Code, "0_Template",sep=computer_sep)
+ dir_Code_Download   <- paste(dir_Code, "1_Download",sep=computer_sep) 
+ dir_Code_Extract    <- paste(dir_Code, "2_Extract",sep=computer_sep) 
+ dir_Code_Analysis   <- paste(dir_Code, "3_Analysis",sep=computer_sep) 
+ dir_Code_Dashboard  <- paste(dir_Code, "4_Dashboard",sep=computer_sep) 
 
+ 
 # Output file tree creator 
-RunsetDir <- function(dir_Output,runset="Test",type="0_Point",create=TRUE,computer_sep="/"){ 
+RunsetDir <- function(dir_Output,runset="Test",type="1_Point",create=TRUE,computer_sep="/"){ 
    #-----------------------------------------------------------------   
    # This both creates the folders from the template 
    # and sets up file locations
@@ -37,7 +40,7 @@ RunsetDir <- function(dir_Output,runset="Test",type="0_Point",create=TRUE,comput
       newdir <- paste(dir_Output,type,runset,sep=computer_sep) 
       if(dir.exists(newdir)){stop(paste("ERROR! Runset: ",type,"/",runset," already exists. Re-name & try again",sep=""))}
       dir.create(newdir)
-      file.copy(paste(dir_Output,type,"Template",sep=computer_sep), 
+      file.copy(from=dir_Output_template, 
                 to=newdir, recursive=TRUE)
    }
 }  
