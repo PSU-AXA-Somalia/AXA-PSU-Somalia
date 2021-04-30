ac630cc379ce14135562eea98fccf3f9df574e67
hgreatrex <5038576+hgreatrex@users.noreply.github.com>
2021-04-30 08:01:00 -0400

    updates download extract code

--- 0_Code/2_Extract/2_Extract_Point_Data.R
+++ 0_Code/2_Extract/2_Extract_Point_Data.R
@@ -7,18 +7,21 @@
  dir_Output_data <- paste(dir_Runset,"0_Data",sep=computer_sep)
  working <- getwd()
  
-#--------------------------------------------------------
+#---------------------------------------------------------------------------------
 # And a function to extract the data
-#--------------------------------------------------------
- extractpoint <- function(filename,location.sp){
-   mydata <- raster(filename)
-   myvalues <- extract(mydata,location.sp)
+#---------------------------------------------------------------------------------
+ extractpoint <- function(filename,location.sp,location,n){
+   e <- extent(location$lon[n]-1, location$lon[n]+1, 
+               location$lat[n]-1, location$lat[n]+1)
+    
+   r <- velox(filename)
+   r$crop(e)
+   return(r$extract_points(location.sp))
  }
 
-
-#--------------------------------------------------------
+#---------------------------------------------------------------------------------
 # Cycle through each product & location
-#--------------------------------------------------------
+#---------------------------------------------------------------------------------
  for(n in 1:nrow(location)){
     print(location[n,])
    
@@ -28,23 +31,30 @@
     satellite_container <- vector(mode="list",length = nrow(InternalSat))
     names(satellite_container) <- InternalSat$Name
    
-   
+    #-----------------------------------------------------------------------------
+    # and extract for each product
+    #-----------------------------------------------------------------------------
     for(s in 1:nrow(InternalSat)){
        setwd(InternalSat$Location[s])
        print(InternalSat$Name[s])
        satout <- data.frame(Date=datelist,Data=NA)
        
+       #--------------------------------------------------------------------------
+       # This could be parallelized to make it go faster
+       #--------------------------------------------------------------------------
        for(d in 1:length(datelist)){
+          if(d %in% seq(from=1,to=100000,by=1000)){print(datelist[d])}
           filename <- paste(InternalSat$PreAddress,
                             format.Date(datelist[d],InternalSat$DateFormat),
                             InternalSat$PostAddress,sep="")
           if(file.exists(filename)){
-             satout$Data[d] <- extractpoint(filename,location.sp)
+             satout$Data[d] <- extractpoint(filename,location.sp,location,n)
           }
        }
        
-       
     } 
  }
+ 
+ 
  
  setwd(working)     
