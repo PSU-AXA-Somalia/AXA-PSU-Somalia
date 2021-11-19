d2838f365cfa2cf691248abf00e52c71656b5cdc
hgreatrex <5038576+hgreatrex@users.noreply.github.com>
2021-11-19 10:44:02 -0500

    spelling error in auto_update_packages
    just tweaking the comment

--- Core/Step0_Global_Parameters.R
+++ Core/Step0_Global_Parameters.R
@@ -25,9 +25,9 @@ globalparams_lastupdated <- as.Date("2021-11-19")
    # and your organisation blocks it].  If you see it constantly fail to download from github,
    # turn to FALSE
    #
-   # If your R version is struggling to install updates, try turning updatepackage <- FALSE
+   # If your R version is struggling to install updates, try turning auto_update_package <- FALSE
    #---------------------------------------------------------------------------------
-    use_github_repos     <- TRUE    
+    use_github_repos     <- FALSE    
     auto_update_packages <- FALSE
     
     #---------------------------------------------------------------------------------
@@ -50,61 +50,6 @@ globalparams_lastupdated <- as.Date("2021-11-19")
    #---------------------------------------------------------------------------------
    # grid : make standardized grid parameters here
      
-
-#=================================================================================
-# 3. DIRECTORIES SUB STRUCTURE - you can edit sub-folder names here. 
-# It will also auto-create any that don't exist, defaults probably fine
-#=================================================================================
-  #---------------------------------------------------------------------------------
-  # Legacy directory separator. It will likely always be "/"
-  # now windows has its act together in R.
-  #---------------------------------------------------------------------------------
-   sep <- "/"       
- 
-  #---------------------------------------------------------------------------------
-  # Main directory (e.g. the location of the github download folder)
-  # You SHOULD be runnnig via your project so it can auto find this    
-  #---------------------------------------------------------------------------------
-  if(("Step0_Core_PROJECTFILE.Rproj" %in% list.files())){
-     dir_main <- substr(getwd(),start=1,stop=nchar(getwd())-5)
-  }else{     
-     if("dir_main" %in% ls()){
-        if(verbose){message("Step 0: you were not in dir_main when running this. Weird.\n Are you debugging?\nChanging main directory back")}
-        setwd(paste(dir_main,"Core",sep=sep))
-     }else{
-        message(paste("I don't think you opened R by double clicking Step0_Core_PROJECTFILE.Rproj\n Your current directory is", 
-                   getwd(),"\n The code is looking for Step0_Core_PROJECTFILE.Rproj in this directory but can only see:"))
-        paste(list.files())
-        stop("CLOSE R-STUDIO.  RE-OPEN R-STUDIO BY DOUBLE CLICKING Step0_Core_PROJECTFILE.Rproj")
-     }
-  }
-
-   #Level 0 
-   dir_core        <- paste(dir_main,"Core",sep=sep)
-   dir_runset      <- paste(dir_main,"Runset",sep=sep)
-
-   #Data Level 1
-   dir_data        <- paste(dir_core,"Data",sep=sep)
-   dir_code        <- paste(dir_core,"Code",sep=sep)
-
-   runset_foldername_rawoutput  <-  "Output1_RawPlots"
-   runset_foldername_visualise   <- "Output2_Visualisation"
-
-  #Data Level 2
-   dir_data_shape           <- paste(dir_data,"1_Shapefiles",    sep=sep)
-   dir_data_remote          <- paste(dir_data,"2_Remote_Sensing",sep=sep)
-
-  #Data Level 3
-   dir_data_remote_ARaw       <- paste(dir_data_remote,"0_Raw_data",sep=sep)
-   dir_data_remote_BGeoTif    <- paste(dir_data_remote,"1_Raw_geoTifs",sep=sep)
-
-  #Data Level 4
-   dir_data_remote_BGeoTif_daily   <-  paste(dir_data_remote_BGeoTif,"A_QC_data_daily",sep=sep)
-   dir_data_remote_BGeoTif_pentad  <-  paste(dir_data_remote_BGeoTif,"B_QC_data_pentad",sep=sep)
-   dir_data_remote_BGeoTif_dekad   <-  paste(dir_data_remote_BGeoTif,"C_QC_data_dekad",sep=sep)
-   dir_data_remote_BGeoTif_month   <-  paste(dir_data_remote_BGeoTif,"D_QC_data_month",sep=sep)
-  
-   
 #=================================================================================
 # And run the setup   
 #=================================================================================
